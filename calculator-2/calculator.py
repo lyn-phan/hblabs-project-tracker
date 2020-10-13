25df6b3c265b8f2e101cb3e687fab26205645041
vagrant <vagrant@vagrant.vm>
2020-10-13 23:03:40 +0000

    Initial Commit

--- calculator-2/calculator.py
+++
@@ -1,7 +0,0 @@
-"""CLI application for a prefix-notation calculator."""
-
-from arithmetic import (add, subtract, multiply, divide, square, cube,
-                        power, mod, )
-
-
-# Replace this with your code
